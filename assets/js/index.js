a8fe34cbc98813a894ca08daed0d7f237bfbd218
alina <a03072006@gmail.com>
2023-11-21 01:02:21 +0200

    add styles

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,9 +10,8 @@ function App() {
     tweets: 1337,
     following: 561,
     followers: 718,
-    /* image:
+    image:
       "https://images.mubicdn.net/images/cast_member/23751/cache-2603-1627313155/image-w856.jpg",
-   */
   };
   return (
     <>
@@ -33,28 +32,51 @@ function UserCard(props) {
     image,
   } = props.user;
   const buttonStyle = {
-    backgroundColor: gender === "female" ? "pink" : "rgb(121, 159, 208)",
+    boxShadow:
+      gender === "female"
+        ? "0px 0px 30px 2px #cc4eb5"
+        : "0px 0px 30px 2px #4f58cc",
+    background:
+      gender === "female"
+        ? "linear-gradient(to bottom, #fc9adb 5%, #d40098 100%)"
+        : "linear-gradient(to bottom, #a09bfa 5%, #0019d4 100%)",
+    backgroundColor: gender === "female" ? "#fc9adb" : "rgb(121, 159, 208)",
+    color: "#fff",
   };
-  /*   const imgStyle = {
-    backgroundImage: `url("${image}")`,
-  }; */
-
+  const cardShadowStyle = {
+    boxShadow:
+      gender === "female"
+        ? "0px 0px 10px 0px #fc9adb"
+        : "0px 0px 10px 0px #a09bfa",
+  };
+  const nicknameStyle = {
+    fontSize: 12,
+    fontStyle: "italic",
+    marginTop: 6,
+  };
+  const nameStyle = {
+    fontSize: 20,
+    fontWeight: "bold",
+  };
+  const statsStyle = {
+    fontSize: 13,
+  };
   return (
-    <div className="user-card">
-      <div className="img-wrapper" /* style={imgStyle} */>
-        {/*  <img src={image} alt={`${name} ${surname}`} /> */}
+    <div className="user-card" style={cardShadowStyle}>
+      <div className="user-avatar">
+        <img src={image} alt={`${name} ${surname}`} />
       </div>
       <button style={buttonStyle}>+</button>
       <div className="user-info">
-        <h1>
+        <h1 style={nameStyle}>
           {name} {surname}
         </h1>
-        <p>{nickname}</p>
+        <p style={nicknameStyle}>{nickname}</p>
       </div>
       <div className="user-stats">
-        <p>Tweets: {tweets}</p>
-        <p>Following: {following}</p>
-        <p>Followers: {followers}</p>
+        <p style={statsStyle}>Tweets: {tweets}</p>
+        <p style={statsStyle}>Following: {following}</p>
+        <p style={statsStyle}>Followers: {followers}</p>
       </div>
     </div>
   );
